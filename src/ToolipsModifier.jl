5bc5c3fcc4f2a52257bf9f37fb8eb4bcd2127cd3
emmy <52672675+emmettgb@users.noreply.github.com>
2022-06-13 17:29:17 -0500

    Add files via upload

--- src/ToolipsModifier.jl
+++ src/ToolipsModifier.jl
@@ -30,7 +30,7 @@ mutable struct Modifier <: ServerExtension
     active_routes::Vector{String}
     on::Function
     function Modifier(active_routes::Vector{String} = ["/"])
-        f(c::Connection, active_routes) = begin
+        f(c::Connection, active_routes = active_routes) = begin
             fullpath = ""
             if contains(c.http.message.target, '?')
                 fullpath = split(c.http.message.target, '?')[1]
@@ -57,7 +57,7 @@ mutable struct Modifier <: ServerExtension
             routes["/modifier/linker"] = document_linker
         end
 
-        function on(f::Function, s::Component, event::String, refs)
+        function on(f::Function, s::Component, event::String, refs = refs)
             ref = gen_ref()
             s["on$event"] = "sendpage($ref);"
             refs[Symbol(ref)] = f
