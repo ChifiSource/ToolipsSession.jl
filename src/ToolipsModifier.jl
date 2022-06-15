3c41bf152f4394f13c8aadd6354e08320d2ab64c
emmy <52672675+emmettgb@users.noreply.github.com>
2022-06-15 15:29:34 -0500

    Add files via upload

--- src/ToolipsModifier.jl
+++ src/ToolipsModifier.jl
@@ -308,7 +308,7 @@ function document_linker(c::Connection)
     s = replace(s, "?CM?:$ref" => "")
     cm = ComponentModifier(s)
     c[:mod].iptable[getip(c)] = now()
-    if ref in keys(c[:mod].events)
+    if getip(c) in keys(c[:mod].events)
         c[:mod][getip(c)][ref](cm)
     else
         write!(c, "timeout")
