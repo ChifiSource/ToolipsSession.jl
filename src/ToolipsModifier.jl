4d4e6f48608a5d400923be1c9083a8632ea1c9ef
emmy <52672675+emmettgb@users.noreply.github.com>
2022-06-15 21:07:53 -0500

    Add files via upload

--- src/ToolipsModifier.jl
+++ src/ToolipsModifier.jl
@@ -14,8 +14,9 @@ This module provides the capability to make web-pages interactive using
 module ToolipsModifier
 
 using Toolips
-import Toolips: ServerExtension, Servable, route!, style!, animate!
-import Toolips: StyleComponent, SpoofConnection
+import Toolips: ServerExtension, route!, style!, animate!, Servable, Connection
+import Toolips: StyleComponent
+#import SpoofConnection, AbstractConnection
 import Base: setindex!, getindex
 using Random, Dates
 
@@ -31,12 +32,15 @@ mutable struct Modifier <: ServerExtension
     f::Function
     active_routes::Vector{String}
     events::Dict
-    iptable::Dict{String, String}
+    iptable::Dict{String, Dates.DateTime}
     timeout::Integer
     function Modifier(active_routes::Vector{String} = ["/"];
         transition_duration::AbstractFloat = 0.5,
-        transition::String = "ease-in-out", timeout::Integer = 10)
+        transition::AbstractString = "ease-in-out", timeout::Integer = 10)
         events = Dict()
+        timeout = timeout
+        transition = transition
+        iptable = Dict{String, Dates.DateTime}()
         f(c::Connection, active_routes = active_routes) = begin
             fullpath = c.http.message.target
             if contains(fullpath, '?')
@@ -44,9 +48,9 @@ mutable struct Modifier <: ServerExtension
             end
             if fullpath in active_routes
                 if ~(getip(c) in keys(events))
-                    events[getip(c)] = Vector{Pair}()
+                    events[getip(c)] = Dict{String, Function}()
                 else
-                    if iptable[getip(c)].minutes >= timeout
+                    if minute(now()) - minute(iptable[getip(c)]) >= timeout
                         delete!(iptable, getip(c))
                     end
                 end
@@ -54,13 +58,16 @@ mutable struct Modifier <: ServerExtension
                 write!(c, """<script>
                 function sendpage(ref) {
                 var ref2 = '?CM?:' + ref;
-                var bodyHtml = document.getElementsByTagName('body')[0].innerHTML;
+            var bodyHtml = document.getElementsByTagName('body')[0].innerHTML;
+                sendinfo(bodyHtml + ref2);
+                }
+                function sendinfo(txt) {
                 let xhr = new XMLHttpRequest();
                 xhr.open("POST", "/modifier/linker");
                 xhr.setRequestHeader("Accept", "application/json");
                 xhr.setRequestHeader("Content-Type", "application/json");
                 xhr.onload = () => eval(xhr.responseText);
-                xhr.send(bodyHtml + ref2);
+                xhr.send(txt);
                 }
                 </script>
                 <style type="text/css">
@@ -78,15 +85,13 @@ mutable struct Modifier <: ServerExtension
             routes["/modifier/linker"] = document_linker
         end
         new([:connection, :func, :routing], f, active_routes, events,
-        Dict{String, String}(), timeout)
+        iptable, timeout)
     end
 end
-getindex(m::Modifier, s::String) = m.events[s]
-setindex!(m::Modifier, a::Function, s::String) = m.events[s] = a
+getindex(m::Modifier, s::AbstractString) = m.events[s]
+getindex(m::Modifier, s::AbstractString) = m.events[s]
 function on(f::Function, c::Connection, s::Component,
-     event::String)
-     Random.seed!( rand(1:100000) )
-     randstring(16)
+     event::AbstractString)
     ref = gen_ref()
     name = s.name
     s["on$event"] = "sendpage('$event$name');"
@@ -95,21 +100,53 @@ end
 
 """
 """
-route!(se::ServerExtension, r::String) = push!(r.active_routes, r)
+route!(se::ServerExtension, r::AbstractString) = push!(r.active_routes, r)
+
+function observe!(f::Function, c::Connection; signal::Bool = false)
+    TimedTrigger(f, time::Integer, signal = false)
+    write!(c, TimedTrigger)
+end
+mutable struct TimedTrigger
+    time::Integer
+    f::Function
+    ref::AbstractString
+    signal::Bool
+    function TimedTrigger(time::Integer, signal::Bool = false)
+        ref = ""
+        f(c::Connection) = begin
+            ref = gen_ref()
+            push!(c[Modifier][get_ip(c)], ref => f)
+            if signal
+                write!(c, """
+                setTimeout(function () {
+                  sendinfo('$ref');
+               }, $time);
+               """)
+            else
+               write!(c, """
+               setTimeout(function () {
+                 sendpage('$ref');
+              }, $time);
+              """)
+            end
+        end
+        new(time, f, ref, signal)
+    end
+end
 
 """
 ### ComponentModifier <: Servable
 A connection Servable is served by the ToolipsModifier.Modifier
-ServerExtension, it is set to modify the  base components (not servables, but
-**servables are planned in a future version**.)
+ServerExtension, it is set to modify the  base components (not Servables, but
+**Servables are planned in a future version**.)
 ###### fields
-- name::String
+- name::AbstractString
 - properties**::Dict** - Properties
 - f**::Function**
 ###### example
 """
 mutable struct ComponentModifier <: Servable
-    html::String
+    html::AbstractString
     f::Function
     changes::Vector{String}
     function ComponentModifier(html)
@@ -123,18 +160,15 @@ mutable struct ComponentModifier <: Servable
 end
 
 function setindex!(cc::ComponentModifier, p::Pair, s::Component)
-    name = s.name
-    value = p[2]
-    prop = p[1]
-    push!(cc.changes, "document.getElementById('$name').$prop = '$value';")
+    modify!(cc, s, p)
 end
 
 function getindex(cc::ComponentModifier, s::Component)
     name = s.name
     tag = s.tag
     s = cc.html
-    tagrange = findall("<$tag id='$name'", s)[1]
-    unsplit_props = s[tagrange[2]:findnext(">", s[tagrange], tagrange[2])]
+    tagrange = findall("<$tag id=\"$name\"", s)[1]
+    unsplit_props = s[tagrange[2]:findnext(">", s, tagrange[2])[1]]
     ps = split(unsplit_props, " ")
     pairs = Vector{Pair{Any, Any}}()
     for p in 1:length(ps)
@@ -146,16 +180,16 @@ function getindex(cc::ComponentModifier, s::Component)
     end
     props = Dict(pairs)
     cname = ""
-    if "id" in props
+    if "id" in keys(props)
         cname = props["id"]
     else
         cname = "unknown"
     end
     props["children"] = Vector()
     props["text"] = ""
-    c = ComponentModifier(cname, tagname, props)
-    endtag = findnext("</$tagname>", s, tagrange[2])
-    if ~(contains(s[tagrange[2]:endtag[1]], "<$tagname>"))
+    c = Component(cname, tag, props)
+    endtag = findnext("</$tag>", s, tagrange[2])
+    if ~(contains(s[tagrange[2]:endtag[1]], "<$tag"))
         if ~(contains(s[tagrange[2]:endtag[1] - 1], "<"))
             c["text"] = s[tagrange[2] + 1:endtag[1] - 1]
         else
@@ -179,20 +213,24 @@ function getindex(cc::ComponentModifier, s::Component)
         comps = makefrom_string(s[tagrange[2] + 1:endtag[1] - 1])
         [push!(c, comp) for comp in comps]
     end
+    return(c)
 end
 
-function makefrom_string(s::String)
+function makefrom_string(s::AbstractString)
+    comps = []
     stap = [start:stop for (start, stop) in zip(findall("<", s), findall(">"))]
     for range in stap
-        tagrange = range[1]:findnext(" ", s, range[1])[1] - 1
-        name_r = findall("id=", s[range]) + 1:
+        name_start = findall("id=", s[range])[1][2] + 1
+        name_end = findnext(" ", s[range], name_sart)[2] - 1
+        tag = s[range[1] + 1:findnext(" ", s, r[1])]
+        push!(comps, s[name_start:name_end])
     end
     [comps = cc[Component(c[1], c[2])] for c in comps]
 end
 
-alert!(cm::ComponentModifier, s::String) = push!(cc.changes, "alert('$s');")
+alert!(cm::ComponentModifier, s::AbstractString) = push!(cc.changes, "alert('$s');")
 
-function redirect!(cm::ComponentModifier, url::String, delay::Int64 = 0)
+function redirect!(cm::ComponentModifier, url::AbstractString, delay::Int64 = 0)
     push!(cm.changes, """
     setTimeout(function () {
       window.location.href = "$url";
@@ -214,20 +252,17 @@ end
 function modify!(cm::ComponentModifier, s::Servable,
     p::Vector{Pair{String, String}})
     name = s.name
-
+    key, val = p[1], p[2]
     push!(cc.changes,
-"document.getElementById('$name').setAttribute('style','$style');")
+"document.getElementById('$name').setAttribute('$key','$val');")
 end
 
-modify!(cm::ComponentModifier, s::Servable, p::Pair)
+modify!(cm::ComponentModifier, s::Servable, p::Pair) = modify!(cm, s, [p])
 
-function add_child!(cm::ComponentModifier, s::Servable, s::Servable, ;
+function add_child!(cm::ComponentModifier, s::Servable, s2::Servable, ;
      at::Integer = 0)
      comp = cm[s]
-     if at == 0
-         at = length(comp[:children])
-     end
-
+     inner = ""
 end
 
 function get_inner()
@@ -242,6 +277,7 @@ function style!(cm::ComponentModifier, s::Servable, p::Pair{String, String} ...)
     p = [pair for pair in p]
     style!(cm::ComponentModifier, s::Servable, p::Pair{String, String})
 end
+
 function style!(cm::ComponentModifier, s::Servable, p::Vector{Pair{String, String}})
     name = s.name
     getelement = "var new_element = document.getElementById('$name');"
@@ -252,6 +288,17 @@ function style!(cm::ComponentModifier, s::Servable, p::Vector{Pair{String, Strin
         push!(c.changes, "new_element.style['$key'] = '$value';")
     end
 end
+
+function remove!(c::Connection, fname::AbstractString, s::Servable)
+    refname = s.name * fname
+    delete!(c[Modifier][get_ip()], refname)
+end
+
+function kill_session!(c::Connection)
+    delete!(c[Modifier].iptable, getip(c))
+    delete!(c[Modifier].refs, get_ip(c))
+end
+
 """
 """
 function document_linker(c::Connection)
@@ -262,8 +309,9 @@ function document_linker(c::Connection)
     s = replace(s, "?CM?:$ref" => "")
     cm = ComponentModifier(s)
     c[:mod].iptable[getip(c)] = now()
-    if ref in
+    if getip(c) in keys(c[:mod].events)
         c[:mod][getip(c)][ref](cm)
+        cm.f(c)
     else
         write!(c, "timeout")
     end
@@ -272,38 +320,38 @@ end
 """
 
 """
-function parse_comphtml(s::String)
+function parse_comphtml(s::AbstractString)
     open_tags = findall("<", s)
     close_tags = findall(">", s)
     # for the future, we can zip this and make it one line with one of these
     open_close = [open_tags[i][1]:close_tags[i][1] for i in 1:length(open_tags)]
-    servables = []
+    Servables = []
     for n in 1:length(open_close)
         tagrange = open_close[n]
 
         if isnothing(endtag)
-            push!(servables, tagrange[2]:maximum(tagrange) + 1 => c)
+            push!(Servables, tagrange[2]:maximum(tagrange) + 1 => c)
         else
-            push!(servables, tagrange[2]:endtag[1] => c)
+            push!(Servables, tagrange[2]:endtag[1] => c)
         end
     end
     news = []
-    for servable in servables
-        if servable[1][2] - 1 == servable[1][1]
-            push!(news, servable[2])
+    for Servable in Servables
+        if Servable[1][2] - 1 == Servable[1][1]
+            push!(news, Servable[2])
             continue
         end
-        for (n, p) in enumerate(servables)
-            if p[1][1] in servable[1]
-                push!(servable[2], p[2])
-                deleteat!(servables, n)
-                push!(news, servable[2])
+        for (n, p) in enumerate(Servables)
+            if p[1][1] in Servable[1]
+                push!(Servable[2], p[2])
+                deleteat!(Servables, n)
+                push!(news, Servable[2])
             else
-                servable[2][:text] = s[servable[1]]
+                Servable[2][:text] = s[Servable[1]]
             end
         end
     end
-    return(Dict([s[2].name => s[2] for s in servables]))
+    return(Dict([s[2].name => s[2] for s in Servables]))
 end
 
 export Modifier, ComponentModifier, on
