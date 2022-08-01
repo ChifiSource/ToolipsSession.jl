4602ac7e44a11e508a86f5da66bdea6a28698b3e
emmy <52672675+emmettgb@users.noreply.github.com>
2022-08-01 01:53:11 -0500

    Add files via upload

--- src/ToolipsSession.jl
+++ src/ToolipsSession.jl
@@ -199,9 +199,7 @@ function observer(f::Function, c::Connection, event::String; time::Integer = 100
         c[:Session][getip(c)] = Dict(event => f)
     end
     obsscript = script(event, text = """
-    setTimeout(function () {
-      sendpage('$event');
-   }, $time);
+    sleep($time); sendpage($event);
    """)
    return(obsscript)
 end
